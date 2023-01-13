2f65ecf161982259ccae3c06692f6caaebd0fdbd
romeroc1996m <camilaromero.9603@gmail.com>
2023-01-13 11:17:30 -0500

    adding js content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 console.log('JavaScript is running!');
+
+/* "Longer JS block comment"
+const is a type of variable - a bit of memory that can hold a value
+
+JS uses (can use) ANY valid CSS selector to maje a connection to ANY element on the page - it works the same way as CSS selectors do, because it's USING css selectors to find matchinh element (s).
+
+The document is the DOM - the web page and all of its elements
+
+querySelector is the method (function) that makes the connection between JS and the DOM (the elements)
+*/
+
+//Step 1 - Make the connection to the element you want to interact with
+
+const jpgGraphic= document.querySelector ("#bitmap");
+
+function logID() {
+    console.log('clicked on this element:', this.id);
+}
+
+// step 2 - decide how you want the user to interact with the object you created in step 1
+
+jpgGraphic.addEventListener('click', logID);
+
+// SVG BUTTON
+const svgGraphic= document.querySelector ("#vector");
+
+function logID() {
+    console.log('clicked on this element:', this.id);
+}
+
+svgGraphic.addEventListener('click', logID);
